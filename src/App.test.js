cb61713404c8c6562934663342e4f050f2e2fa5c
Aditya Satendra Dixit <ad433393@dal.ca>
2022-04-03 04:59:19 -0300

    Revert "Merge branch 'HotFix' into 'main'"
    This reverts merge request !21

--- src/App.test.js
+++ src/App.test.js
@@ -1,19 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
-import {Provider} from 'react-redux';
-import reducer from './Reducers/reducer'
-import {createStore} from 'redux';
-import {loadState, saveState} from './localStorage'
-
-const persistedState=loadState()
-const store=createStore(reducer,persistedState)
-
-store.subscribe(()=>{
-    saveState(store.getState())
-})
 
 test('renders learn react link', () => {
-  render(<Provider store={store}><App /></Provider>);
-  const linkElement = screen.getByText(/scrum/i);
+  render(<App />);
+  const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
