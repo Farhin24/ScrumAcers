43e353062b374705464cf829ea22df4aa92fd095
adixit <ad433393@dal.ca>
2022-04-05 20:36:24 -0300

    Survey Form Test Suite Update

--- cypress/integration/SurveyForm/SurveyFormEmployee.spec.js
+++ cypress/integration/SurveyForm/SurveyFormEmployee.spec.js
@@ -1,23 +1,23 @@
 
 
-describe("Stand_Up_Form_Employee",() =>{  
+describe("Survey_Form_Employee Test Suite",() =>{  
 
     it("Check Route StandUpForm",()=>{
         cy.login_employee()
-        cy.visit("http://localhost:3000/SurveyFormParent")
+        cy.visit("http://scrum-acers-frontend.herokuapp.com/SurveyFormParent")
         expect(cy.findByText(/Survey Forms/i))
     })
     
     it("Check Form Status", () =>{
             cy.login_employee()
-            cy.visit("http://localhost:3000/SurveyFormParent")
-            cy.get('button[name="title"]')
+            cy.visit("http://scrum-acers-frontend.herokuapp.com/SurveyFormParent")
+            cy.get('.css-morr9g')
     })
 
     it("Check Responses",()=>{
         cy.login_employee()
-        cy.visit("http://localhost:3000/SurveyFormParent")
-        cy.get('button[name="title"]').click()
+        cy.visit("http://scrum-acers-frontend.herokuapp.com/SurveyFormParent")
+        cy.get('.css-morr9g').click()
         cy.get('textarea[name="question1"]').type("test")
         cy.get('textarea[name="question2"]').type("test")
         cy.get('textarea[name="question3"]').type("test")
